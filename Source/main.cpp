ac6a575cc8c49f1522ccb3c2a4a09868edaa342e
Your Name <you@example.com>
2022-09-07 17:19:12 +0300

    Antialiasing
    Antialiasing technique is applied to make edges smooth. For each pixel,
    more than one ray was sampled. The color of that pixel was given as the
    average of the colors of these samples.

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,6 +3,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 // FUCNTIONS
 color ColorRay(const ray& r, const hittable& world);
@@ -16,7 +17,7 @@ color ColorRay(const ray& r, const hittable& world) {
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0); //background
 }
 
 int main()
@@ -25,35 +26,31 @@ int main()
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
 
 	// World
 	hittable_list world;
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(make_shared<sphere>(point3(0, 1, -1), 0.3));
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	// Camera
 	// (-2, 1, -1): left-top corner, (2, -1, -1): right-bottom corner
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0; // distance between the projection plane and the projection point
+	camera cam;
 
-	auto origin = point3(0, 0, 0); // camera position
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
-	
 	// MAIN RENDER LOOP
 	// Create ppm format file, image
 	//left-top corner: (0,0), right-bottom corner: (1,1)
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 	for (int j = image_height - 1; j >= 0; --j) { 
 		for (int i = 0; i < image_width; ++i) {
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ColorRay(r, world);
-			write_color(std::cout, pixel_color);
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) { //multiple sample for a pixel
+				auto u = (i + random_double()) / (image_width - 1);
+				auto v = (j + random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ColorRay(r, world); //multi-sampled color computation
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 	return 0;
